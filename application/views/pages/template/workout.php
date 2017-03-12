099f05b446ab134adc002edcd7db6ee8e5f3d0ab
HakanBergman <hakan.bergman@drumedar.se>
2017-03-12 23:18:47 +0100

    Updated

--- application/views/pages/template/workout.php
+++ application/views/pages/template/workout.php
@@ -1,25 +1,13 @@
 <div class="box schedules blue">
-  <h1> Alla Grupper </h1>
+  <h1> Pass </h1>
 </div>
 
 <table class="box schedules" id="sort_groups">
   <tbody id="tabledivbody">
-    <tr>
-      <td colspan="4">
-        <a href="/group/add">
-          Lägg till grupp
-        </a>
-      </td>
-    </tr>
     {foreach from=$groups item=cur key=key}
     <tr class="sortable sectionsid" id="sectionsid_{$cur->group_id}">
       <td>{$cur->title}</td>
       <td style="width: 64px; text-align: center; color: darkgray; font-size: smaller;" class="tooltip"></td>
-      {*<td style="width: 32px;" onmouseout="$('.tooltip').eq({$key}).text('');" onmouseover="$('.tooltip').eq({$key}).text('Visa utövare');">
-        <a href="/group/users/{$cur->group_id}">
-          <img src="/assets/images/users.png" />
-        </a>
-      </td>*}
       <td style="width: 32px;" onmouseout="$('.tooltip').eq({$key}).text('');" onmouseover="$('.tooltip').eq({$key}).text('Ändra egenskaper');">
         <a href="/group/edit/{$cur->group_id}">
           <img src="/assets/images/edit.png" />
@@ -35,14 +23,14 @@
       </td>
     </tr>
     {/foreach}
+  </tbody>
+  <tfoot>
     <tr>
-      <td colspan="4">
-        <a href="/group/add">
-          Lägg till grupp
-        </a>
+      <td colspan="4" style="background-color: #cccccc;">
+        <button>Lägg till pass</button>
       </td>
     </tr>
-  </tbody>
+  </tfoot>
 </table>
 
 
