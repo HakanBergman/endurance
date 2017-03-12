cda24404fd164f6c8a533890e2c780ca46b6f9da
HakanBergman <hakan.bergman@drumedar.se>
2017-03-13 00:48:13 +0100

    Updated

--- application/models/workout_model.php
+++ application/models/workout_model.php
@@ -26,7 +26,7 @@ class Workout_model extends MY_Model {
                     workout_parts.part_id = parts.id AND
                     workout_parts.workout_id = ?
                 ORDER BY
-                    workout_parts.order ASC";
+                    workout_parts.order DESC";
             $values = array($workout_id);
             $parts = $this->db->query($sql, $values)->result();
         }
@@ -160,7 +160,7 @@ class Workout_model extends MY_Model {
     }
     
     function get_workout($id) {
-         $sql = "SELECT * FROM `workouts` WHERE `id` = ? ORDER BY display_order asc LIMIT 0,1";
+         $sql = "SELECT * FROM `workouts` WHERE `id` = ? LIMIT 0,1";
          $values = array($id);
 
          $query = $this->db->query($sql, $values);
