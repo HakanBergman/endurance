205804361a703adfd19ac2bf9f333cba3319a0e9
HakanBergman <hakan.bergman@drumedar.se>
2017-03-12 23:16:39 +0100

    Update

--- application/views/pages/template/workout.php
+++ application/views/pages/template/workout.php
@@ -1,3 +1,51 @@
+<div class="box schedules blue">
+  <h1> Alla Grupper </h1>
+</div>
+
+<table class="box schedules" id="sort_groups">
+  <tbody id="tabledivbody">
+    <tr>
+      <td colspan="4">
+        <a href="/group/add">
+          Lägg till grupp
+        </a>
+      </td>
+    </tr>
+    {foreach from=$groups item=cur key=key}
+    <tr class="sortable sectionsid" id="sectionsid_{$cur->group_id}">
+      <td>{$cur->title}</td>
+      <td style="width: 64px; text-align: center; color: darkgray; font-size: smaller;" class="tooltip"></td>
+      {*<td style="width: 32px;" onmouseout="$('.tooltip').eq({$key}).text('');" onmouseover="$('.tooltip').eq({$key}).text('Visa utövare');">
+        <a href="/group/users/{$cur->group_id}">
+          <img src="/assets/images/users.png" />
+        </a>
+      </td>*}
+      <td style="width: 32px;" onmouseout="$('.tooltip').eq({$key}).text('');" onmouseover="$('.tooltip').eq({$key}).text('Ändra egenskaper');">
+        <a href="/group/edit/{$cur->group_id}">
+          <img src="/assets/images/edit.png" />
+        </a>
+      </td>
+      <td style="width: 32px;" onmouseout="$('.tooltip').eq({$key}).text('');" onmouseover="$('.tooltip').eq({$key}).text('Ta bort gruppen');">
+        <a href="/group/delete/{$cur->group_id}">
+          <img src="/assets/images/remove.png" />
+        </a>
+      </td>
+      <td style="width: 42px;">
+        <img src="/assets/images/down.png" class="movedownlink" />&nbsp;&nbsp;<img src="/assets/images/up.png" class="moveuplink" />
+      </td>
+    </tr>
+    {/foreach}
+    <tr>
+      <td colspan="4">
+        <a href="/group/add">
+          Lägg till grupp
+        </a>
+      </td>
+    </tr>
+  </tbody>
+</table>
+
+
 
 <div class="box schedules blue">
 
@@ -21,7 +69,7 @@
         var row = function (info) {
 
         var $row = $('<tr class="sortable sectionsid">' +
-            '<td>' + info.__string__ + ' <span style="color: #999;">' + info.__comment__ + '</span></td>' +
+            '"<td>' + info.__string__ + ' <span style="color: #999;">' + info.__comment__ + '</span></td>' +
             '<td style="width: 64px; text-align: center; color: darkgray; font-size: smaller;" class="tooltip"></td>' +
             '<td style="width: 32px;" onmouseout="' + "$(this).parent().find('.tooltip').text('');" + '" onmouseover="' + "$(this).parent().find('.tooltip').text('Ändra egenskaper');" + '"><img src="/assets/images/edit.png" /></td>' +
             '<td style="width: 32px;" onmouseout="' + "$(this).parent().find('.tooltip').text('');" + '" onmouseover="' + "$(this).parent().find('.tooltip').text('Ta bort passet');" + '"><img src="/assets/images/remove.png" /></td>' +
---
+++ updates/1.0/update_table_workout.sql
@@ -0,0 +1,2 @@
+﻿/* Add display order to workouts */
+ALTER TABLE workouts ADD display_order int(11) DEFAULT 0;
