8e5fe9e2382033925f8bb021072cb5595037add3
HakanBergman <hakan.bergman@drumedar.se>
2017-03-13 01:23:09 +0100

    Updated

--- application/views/pages/template/workout.php
+++ application/views/pages/template/workout.php
@@ -8,7 +8,7 @@
     {* Why not controller/workout? TODO *}
     {foreach from=$template_workout item=cur key=key}
       {$cur|@var_dump}
-      <tr class="sortable sectionsid" id="sectionsid_{$cur->group_id}">
+      <tr class="sortable sectionsid" id="sectionsid_{$cur->workout_id}">
         <td>{$cur->__string__} <span style="color: #999;">{$cur->comment}</span></td>
         <td style="width: 64px; text-align: center; color: darkgray; font-size: smaller;" class="tooltip"></td>
         <td style="width: 32px;"><img src="/assets/images/edit.png" alt="Ändra egenskaper" /></td>
