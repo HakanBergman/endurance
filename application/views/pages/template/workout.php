4def2e4a5e279c3d38c553520a0a8bfc0b84491c
HakanBergman <hakan.bergman@drumedar.se>
2017-03-13 01:22:28 +0100

    Updated

--- application/views/pages/template/workout.php
+++ application/views/pages/template/workout.php
@@ -11,19 +11,9 @@
       <tr class="sortable sectionsid" id="sectionsid_{$cur->group_id}">
         <td>{$cur->__string__} <span style="color: #999;">{$cur->comment}</span></td>
         <td style="width: 64px; text-align: center; color: darkgray; font-size: smaller;" class="tooltip"></td>
-        <td style="width: 32px;" onmouseout="$('.tooltip').eq({$key}).text('');" onmouseover="$('.tooltip').eq({$key}).text('Ändra egenskaper');">
-          <a href="/group/edit/{$cur->group_id}">
-            <img src="/assets/images/edit.png" />
-          </a>
-        </td>
-        <td style="width: 32px;" onmouseout="$('.tooltip').eq({$key}).text('');" onmouseover="$('.tooltip').eq({$key}).text('Ta bort gruppen');">
-          <a href="/group/delete/{$cur->group_id}">
-            <img src="/assets/images/remove.png" />
-          </a>
-        </td>
-        <td style="width: 42px;">
-          <img src="/assets/images/down.png" class="movedownlink" />&nbsp;&nbsp;<img src="/assets/images/up.png" class="moveuplink" />
-        </td>
+        <td style="width: 32px;"><img src="/assets/images/edit.png" alt="Ändra egenskaper" /></td>
+        <td style="width: 32px;"><img src="/assets/images/remove.png" alt="Ta bort passet" /></td>
+      </tr>');
       </tr>
     {/foreach}
   </tbody>
